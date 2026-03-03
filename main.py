3adfd110de308fe6a0d59d1e6e999e2254658654
user <user@MSI-NB138XRY.mastersystem.co.id>
2026-03-03 15:46:39 +0700

    coma

--- main.py
+++ main.py
@@ -3,49 +3,87 @@ import pandas as pd
 import random
 import re
 
-df = pd.read_csv('input/vEdge-02-18-2026.csv')
+df = pd.read_csv('input/vEdge-02-24-2026.csv')
 print(df)
 print('\n')
 
 new_data = []
+DC = []
+DRC = []
 
 for row in df.itertuples(index=False):
-    hostname = row[1]
-    sn = row[4]
-    system_ip = row[5]
+    hostname = row[2]
+    sn = row[6]
+    system_ip = row[7]
 
     if isinstance(system_ip, float):
         continue
-    else:      
+    else:
+        DC_patt = '^DC'      
+        DRC_patt = '^DRC'      
         bank_code = re.split('\\.', system_ip)
-        DC_NAT_RTGS = f'172.20.{bank_code[2]}.232/29, 172.20.{bank_code[2]}.248/29'
-        DC_NAT_Pelaporan = f'172.20.{bank_code[2]}.0/26, 172.20.{bank_code[2]}.64/27, 172.20.{bank_code[2]}.96/27, 172.20.{bank_code[2]}.160/27'
-        DRC_NAT_RTGS = f'172.19.{bank_code[2]}.232/29, 172.19.{bank_code[2]}.248/29'
+        DC_NAT_RTGS = f'172.20.{bank_code[2]}.232/29, 172.20.{bank_code[2]}.248/29, '
+        DC_NAT_Pelaporan = f'172.20.{bank_code[2]}.0/26, 172.20.{bank_code[2]}.64/27, 172.20.{bank_code[2]}.96/27, 172.20.{bank_code[2]}.160/27, '
+        DRC_NAT_RTGS = f'172.19.{bank_code[2]}.232/29, 172.19.{bank_code[2]}.248/29, '
         DRC_NAT_Pelaporan = f'172.19.{bank_code[2]}.0/26, 172.19.{bank_code[2]}.64/27, 172.19.{bank_code[2]}.96/27, 172.19.{bank_code[2]}.160/27'
+
+        DC_match = re.match(DC_patt, hostname, flags=0)
+        DRC_match = re.match(DC_patt, hostname, flags=0)
 
         data = {
             'Hostname': hostname,
             'SN': sn,
             'System IP': system_ip,
             'Bank Code': bank_code[2],
-            'DC_NAT_RTGS': DC_NAT_RTGS,
-            'DC_NAT_Pelaporan': DC_NAT_Pelaporan,
-            'DRC_NAT_RTGS': DRC_NAT_RTGS,
-            'DRC_NAT_Pelaporan': DRC_NAT_Pelaporan
+            'DC_NAT_RTGS dan Pelaporan': f'{DC_NAT_RTGS} {DC_NAT_Pelaporan}',
+            'DRC_NAT_RTGS dan Pelaporan': f'{DRC_NAT_RTGS} {DRC_NAT_Pelaporan}',
         }
 
         new_data.append(data)
 
-        # print(f'system ip: {system_ip} and bank code: {bank_code[2]}')
-        # print(f'bank code: {bank_code[2]}')
-        # print(f'DC_NAT_RTGS: {DC_NAT_RTGS}')
-        # print(f'DC_NAT_Pelaporan: {DC_NAT_Pelaporan}')
-        # print(f'DRC_NAT_RTGS: {DRC_NAT_RTGS}')
-        # print(f'DRC_NAT_Pelaporan: {DRC_NAT_Pelaporan}')
-        # print('\n')
+        # if DC_match:
+        #     DC_data = {
+        #         'Hostname': hostname,
+        #         'SN': sn,
+        #         'System IP': system_ip,
+        #         'Bank Code': bank_code[2],
+        #         'DC_NAT_RTGS dan Pelaporan': f'{DC_NAT_RTGS} {DC_NAT_Pelaporan}',
+        #         'DRC_NAT_RTGS dan Pelaporan': f'{DRC_NAT_RTGS} {DRC_NAT_Pelaporan}',
+        #     }
+
+        #     DC.append(DC_data)
+
+        #     # print(f'system ip: {system_ip} and bank code: {bank_code[2]}')
+        #     # print(f'bank code: {bank_code[2]}')
+        #     # print(f'DC_NAT_RTGS: {DC_NAT_RTGS}')
+        #     # print(f'DC_NAT_Pelaporan: {DC_NAT_Pelaporan}')
+        #     # print(f'DRC_NAT_RTGS: {DRC_NAT_RTGS}')
+        #     # print(f'DRC_NAT_Pelaporan: {DRC_NAT_Pelaporan}')
+        #     # print('\n')
+
+        # if DRC_match:
+        #     DRC_data = {
+        #         'Hostname': hostname,
+        #         'SN': sn,
+        #         'System IP': system_ip,
+        #         'Bank Code': bank_code[2],
+        #         # 'DC_NAT_RTGS dan Pelaporan': f'{DC_NAT_RTGS} {DC_NAT_Pelaporan}',
+        #         'DRC_NAT_RTGS dan Pelaporan': f'{DRC_NAT_RTGS} {DRC_NAT_Pelaporan}',
+        #     }
+
+        #     DRC.append(DRC_data)
+
+# print(DC_data)
+# print(DRC_data)
+
+# DC_data_df = pd.DataFrame(data=DC_data)        
+# DRC_data_df = pd.DataFrame(data=DRC_data)
+
+# print(DC_data_df)
+# print(DRC_data_df)
 
 new_df = pd.DataFrame(data=new_data,)
-new_df.to_csv('output/new_data.csv', index=False)
+new_df.to_csv('output/new_data_2.csv', index=False)
 
 print(new_df)
 
